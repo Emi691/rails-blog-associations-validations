81d421a5ef6e2a860758e01156713ec4af580f47
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2021-03-18 16:15:01 -0700

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,17 @@
 class Post < ActiveRecord::Base
-  
+    validates :name, presence: true
+    validates :content, presence: true
+
+    belongs_to :user
+    has_many :post_tags
+    has_many :tags, through: :post_tags
+
+    # accepts_nested_attributes_for :tags
+
+    def tags_attributes=(tag_attributes)
+        tag_attributes.values.each do |tag_attribute|
+            tag = Tag.find_or_create_by(tag_attribute)
+            self.tags << tag
+        end
+    end
 end
